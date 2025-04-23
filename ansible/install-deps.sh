a8769457e567a1ba5bd6418b2cb501e2184f11c7
Luis Pinillos <pinillosluisfer@gmail.com>
2025-04-23 03:28:16 -0500

    test

--- ansible/install-deps.sh
+++ ansible/install-deps.sh
@@ -5,9 +5,10 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 
 # Docker Compose
-mkdir -p /usr/local/lib/docker/cli-plugins
-curl -SL https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-linux-x86_64 -o /usr/local/lib/docker/cli-plugins/docker-compose
-chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
 
-# Habilitar Docker sin sudo
+# Habilitar Docker para el usuario actual
 usermod -aG docker azureuser
