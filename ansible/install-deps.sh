2bd286740d0f30fdfad283931a221ab08658aa69
Luis Pinillos <pinillosluisfer@gmail.com>
2025-04-23 03:17:20 -0500

    changes in terraform and ansible

--- ansible/install-deps.sh
+++ ansible/install-deps.sh
@@ -5,10 +5,9 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 
 # Docker Compose
-DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
-mkdir -p $DOCKER_CONFIG/cli-plugins
-curl -SL https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
-chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+mkdir -p /usr/local/lib/docker/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-linux-x86_64 -o /usr/local/lib/docker/cli-plugins/docker-compose
+chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
 
-# Habilitar Docker para el usuario actual
+# Habilitar Docker sin sudo
 usermod -aG docker azureuser
