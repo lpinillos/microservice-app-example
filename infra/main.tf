37b2b9e137beb39b804512d988182e8f53ddc3a0
Luis Pinillos <pinillosluisfer@gmail.com>
2025-04-22 22:17:40 -0500

    feat: nsg added to terraform

--- infra/main.tf
+++ infra/main.tf
@@ -67,3 +67,27 @@ resource "azurerm_linux_virtual_machine" "vm" {
     version   = "latest"
   }
 }
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-devops"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
