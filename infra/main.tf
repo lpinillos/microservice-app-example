e235cb994595c7374931702c5103a701c5defc9d
Luis Pinillos <pinillosluisfer@gmail.com>
2025-04-25 11:58:43 -0500

    test

--- infra/main.tf
+++ infra/main.tf
@@ -171,6 +171,18 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+    security_rule {
+    name                       = "port-80"
+    priority                   = 1009
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
