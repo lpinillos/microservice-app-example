77cfad240904eadfd789eb26e58541a90dc39ce3
Luis Pinillos <102693827+lpinillos@users.noreply.github.com>
2025-04-25 01:39:54 -0500

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,5 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-// test
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm'
 import 'bootstrap/dist/css/bootstrap.css'
