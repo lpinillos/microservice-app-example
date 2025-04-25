e39cee974881ccbbcd0e282081c8d72b882e5ba8
Luis Pinillos <pinillosluisfer@gmail.com>
2025-04-25 00:57:19 -0500

    testing deployment

--- infra/config.sh
+++ infra/config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-apt-get update
-apt-get install -y docker.io docker-compose git
+sudo apt-get update
+sudo apt-get install -y docker.io docker-compose git
 
-usermod -aG docker azureuser
+sudo usermod -aG docker azureuser
 
 cd /home/azureuser
 git clone https://github.com/lpinillos/microservice-app-example.git
 cd microservice-app-example
 
-docker-compose -f docker-compose.yml up -d --build
+sudo docker-compose -f docker-compose.yml up -d --build
